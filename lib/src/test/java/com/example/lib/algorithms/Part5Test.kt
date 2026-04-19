8d4b68b440baae815711e798b63b08f754acebb0
Ihor Klimov <iklimov6@gmail.com>
2026-04-19 09:38:22 -0400

    Refactor

--- lib/src/test/java/com/example/lib/algorithms/Part5Test.kt
+++ lib/src/test/java/com/example/lib/algorithms/Part5Test.kt
@@ -49,10 +49,10 @@ class Part5Test {
 private fun countNumOfOnes(number: Int): Int {
     var result = 0
 
-    var remainder = number
+    var n = number
 
-    while (remainder != 0) {
-        remainder = remainder and (remainder - 1)
+    while (n != 0) {
+        n = n and (n - 1)
         result++
     }
 
